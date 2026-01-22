814aed12d0d88052ecd4e76941f7c46fafdd1078
zie-ning <jeeun03@gmail.com>
2026-01-22 11:29:31 +0900

    chore: 초기 세팅, 의존성 주입

---
+++ src/main/java/com/wink/gongongu/GongonguApplication.java
@@ -0,0 +1,13 @@
+package com.wink.gongongu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GongonguApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GongonguApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wink/gongongu/GongonguApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wink.gongongu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GongonguApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
