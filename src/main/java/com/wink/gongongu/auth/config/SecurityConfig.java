a0ebd30860595b20e945234faaa14761aff0e3d9
zie-ning <jeeun03@gmail.com>
2026-02-03 21:26:05 +0900

    feat: oauth 로그인 시 쿠키에 로그인 토큰 등록

--- src/main/java/com/wink/gongongu/auth/config/SecurityConfig.java
+++ src/main/java/com/wink/gongongu/auth/config/SecurityConfig.java
@@ -11,6 +11,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @RequiredArgsConstructor
@@ -24,6 +27,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .csrf(csrf -> csrf.disable())
             .formLogin(fl -> fl.disable())
             .httpBasic(hb -> hb.disable())
@@ -53,5 +57,18 @@ public class SecurityConfig {
             .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
--- src/main/java/com/wink/gongongu/auth/handler/OAuth2SuccessHandler.java
+++ src/main/java/com/wink/gongongu/auth/handler/OAuth2SuccessHandler.java
@@ -1,11 +1,14 @@
 package com.wink.gongongu.auth.handler;
 
-import com.wink.gongongu.auth.dto.CustomOAuth2User;
+import com.wink.gongongu.auth.jwt.service.JwtTokenProvider;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -16,9 +19,11 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @Component
+@RequiredArgsConstructor
 public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
     private final RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Value("${app.front-base-url}")
     private String frontBaseUrl;
@@ -29,6 +34,8 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
         OAuth2User oauth2User = (OAuth2User) authentication.getPrincipal();
         boolean isTmpUser = hasAuthority(oauth2User, "ROLE_TMP");
+        String loginToken = jwtTokenProvider.createLoginToken(Long.valueOf(oauth2User.getName()));
+        addLoginTokenCookie(response,loginToken);
 
         if(isTmpUser){ //임시 유저일 경우 회원가입 프론트 URL로 리다이렉트
             String targetUrl = UriComponentsBuilder.fromUriString(frontBaseUrl)
@@ -56,5 +63,17 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
             }
         }
         return false;
+    }
+
+    private void addLoginTokenCookie(HttpServletResponse response, String token) {
+        ResponseCookie cookie = ResponseCookie.from("LOGIN_TOKEN", token)
+            .httpOnly(true)
+            .secure(true)
+            .sameSite("None")
+            .path("/")
+            .maxAge(86400) // 1일
+            .build();
+
+        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
     }
 }
