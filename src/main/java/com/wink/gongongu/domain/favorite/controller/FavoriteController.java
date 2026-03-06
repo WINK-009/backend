d363634b58da741652b9c43eb62ea78dffe4405d
Jeongdaeun1 <daeun1q2w3e@gmail.com>
2026-03-07 01:34:59 +0900

    feat: 찜하기, 찜 취소, 찜 목록 조회기능

---
+++ src/main/java/com/wink/gongongu/domain/favorite/controller/FavoriteController.java
@@ -0,0 +1,40 @@
+package com.wink.gongongu.domain.favorite.controller;
+
+import com.wink.gongongu.domain.favorite.service.FavoriteService;
+import com.wink.gongongu.domain.post.dto.PostListResponse;
+import com.wink.gongongu.domain.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class FavoriteController {
+    private final FavoriteService favoriteService;
+
+    @PostMapping("/posts/{postId}/fav")
+    public ResponseEntity<Void> addFav(
+            @AuthenticationPrincipal User user,
+            @PathVariable Long postId
+    ){
+        favoriteService.addFavorite(user.getId(), postId);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/posts/{postId}/fav")
+    public ResponseEntity<Void> deleteFav(
+            @AuthenticationPrincipal User user,
+            @PathVariable Long postId
+    ){
+        favoriteService.deleteFavorite(user.getId(), postId);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/users/fav")
+    public ResponseEntity<List<PostListResponse>> myFav(@AuthenticationPrincipal User user){
+        return ResponseEntity.ok(favoriteService.findFavPosts(user.getId()));
+    }
+}
--- src/main/java/com/wink/gongongu/domain/post/repository/PostRepository.java
+++ src/main/java/com/wink/gongongu/domain/post/repository/PostRepository.java
@@ -33,4 +33,6 @@ public interface PostRepository extends JpaRepository<Post, Long> {
             @Param("status") PostStatus status,
             Pageable pageable
     );
+
+    Post findByPostId(Long postId);
 }
