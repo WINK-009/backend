a1a95232b943d2c9ce19e83377e964a0ea596a8c
overhyeon <pjrlks2002@gmail.com>
2026-03-09 23:05:28 +0900

    fix: JPA Auditing 중복 설정 제거

--- src/main/java/com/wink/gongongu/GongonguApplication.java
+++ src/main/java/com/wink/gongongu/GongonguApplication.java
@@ -2,9 +2,7 @@ package com.wink.gongongu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class GongonguApplication {
 
@@ -13,3 +11,4 @@ public class GongonguApplication {
 	}
 
 }
+
